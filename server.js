699608cd0b2329bc9da45ed44436a2b7ca32967f
Lidwi Kurniawan <incogboy@live.com>
2023-11-17 11:40:37 +0700

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoute from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoute from './routes/productRoute.js'
 import cors from 'cors'
+import path from 'path'
 
 //configure env
 dotenv.config()
@@ -21,6 +22,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use('/api/v1/auth', authRoute)
@@ -28,10 +30,13 @@ app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/products', productRoute)
 
 //rest api
-app.get('/', (req, res) => {
-    res.send({
-        message: 'Welcome'
-    })
+// app.get('/', (req, res) => {
+//     res.send({
+//         message: 'Welcome'
+//     })
+// })
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 //PORT
